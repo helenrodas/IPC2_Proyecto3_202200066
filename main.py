595e777d0f7c445874064b865b172cec4ae008d2
helenrodas <3030360790108@ingenieria.usac.edu.gt>
2023-10-23 21:29:51 -0600

    se lee archivo de entrada

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask,jsonify,request
+from flask_cors import CORS
+import base64
+app= Flask(__name__)
+CORS(app)
+
+@app.route('/upload_file',methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({
+        "message": "Error! No se ha enviado archivo"
+        })
+    file = request.files['file']
+    file_content=file.read().decode('utf-8')
+    print(file_content)
+    return jsonify({
+        "message": "El archivo fue cargado y leido exitosamente"
+        })  
+
+
+if __name__=="__main__":
+    app.run(threaded=True,port=5000,debug=True)
